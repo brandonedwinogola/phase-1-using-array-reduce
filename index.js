53e5f7e02ad9f99fb97241bee672e04fc39f86b6
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-12 12:19:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+fuction totalBatteries(batteryBatches) {
+    let totalBatches = 0;
+
+    for (const batteryBatches of totalBatteries) {
+        totalBatteries += batteryBatches.value;
+    };
+
+    return totalBatteries;
+}
+
+console.log(ourReduce(batteryBatches, batchesCounter, 0)); // LOG:31
 // Code your solution here
